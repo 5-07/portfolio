561da6d9df42e993795d8a9985b917ea5edd8655
Muzaina Munir <muzainamunirsiddiqui@gmail.com>
2026-05-24 23:28:52 +0500

    Update Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -84,7 +84,7 @@ export default function Contact() {
   >
     SEND EMAIL
   </a>
-  
+  <a
     href="https://wa.me/92XXXXXXXXXX?text=Hi%20Muzaina%2C%20I%20found%20your%20portfolio%20and%20would%20love%20to%20discuss%20a%20project."
     target="_blank"
     rel="noopener noreferrer"
